7a63ce4eec8356d8b1d9e73b0cb26742a5340eaf
Vimal Venugopal <vimalalwaysrocks@gmail.com>
2023-08-31 16:51:23 -0400

    new

---
+++ index_files/menucontents.js
@@ -0,0 +1,97 @@
+
+var anylinkmenu1={divclass:'anylinkmenu', inlinestyle:'', linktarget:''} //First menu variable. Make sure "anylinkmenu1" is a unique name!
+anylinkmenu1.items=[
+	[sObject['ALLPRODUCTS'], "../../navigation.cfm"],
+	[sObject['IMAGING'], "../../navigation.cfm?guide_id=2268"],
+	[sObject['OPTOMECHANICS'], "../../navigation.cfm?guide_id=2262"],
+	[sObject['MOTIONCONTROL'], "../../navigation.cfm?guide_id=2263"],
+	[sObject['OPTICS'], "../../navigation.cfm?guide_id=2264"],
+	[sObject['FIBER'], "../../navigation.cfm?guide_id=2265"],
+	[sObject['LIGHT'], "../../navigation.cfm?guide_id=2266"],
+	[sObject['LIGHTANALYSIS'], "../../navigation.cfm?guide_id=2267"],
+	[sObject['APPLICATIONS'], "../../navigation.cfm?guide_id=2315"],	
+	[sObject['NEWPRODUCTS'], "../../newproducts/newproducts.cfm?clearFilters"],
+	[sObject['DOWNLOAD'], "../../navigation.cfm?Guide_ID=2191"],
+	[sObject['TUTORIALS'], "../../navigation.cfm?Guide_ID=2400"]
+]
+
+var anylinkmenu2={divclass:'anylinkmenu', inlinestyle:'', linktarget:''} //Second menu variable. Same precaution.
+anylinkmenu2.items=[
+	[sObject['CUSTOM'], "../../navigation.cfm?guide_id=2312"],
+	[sObject['TECHSUPPORT'], "../../SupportContact.cfm"],
+	[sObject['RETURN'], "../../support.cfm?section=7&viewTab=3"],
+	[sObject['DOWNLOAD'], "../../navigation.cfm?Guide_ID=2191"],
+	[sObject['TUTORIALS'], "../../navigation.cfm?Guide_ID=2400"],
+	[sObject['TERMANDCONDITION'], "../../support.cfm?section=7&viewTab=1"],
+	[sObject['DISCOUNT'], "../../discountPolicy.cfm"],
+	[sObject['QUALITYSYSTEMS'], "../../support.cfm?section=7&viewtab=5"],
+	[sObject['SUPPLIERS'], "../../suppliers.cfm"]
+]
+var anylinkmenu9={divclass:'anylinkmenu', inlinestyle:'', linktarget:''} //Second menu variable. Same precaution.
+anylinkmenu9.items=[
+    [sObject['CUSTOM'], "../../navigation.cfm?guide_id=2312"],
+    [sObject['DOWNLOAD'], "../../navigation.cfm?Guide_ID=2191"],
+    [sObject['TERMANDCONDITION'], "../../support.cfm?section=7&viewTab=1"],
+    [sObject['RETURN'], "../../support.cfm?section=7&viewTab=3"],
+    [sObject['TECHSUPPORT'], "../../SupportContact.cfm"]
+]
+
+var anylinkmenu3={divclass:'anylinkmenu', inlinestyle:'', linktarget:''} //Third menu variable. Same precaution.
+anylinkmenu3.items=[
+	[sObject['FOOD4THOUGHT'], "../../GeneralFeedback.cfm"],
+	[sObject['MANULOC'], "../../locations.cfm"],
+	[sObject['INTERDIST'], "../../distributors.cfm"],
+	[sObject['CAREER'], "../../careersbycountry.cfm"],
+	[sObject['ABOUTUS'], "../../about_us.cfm"],
+	[sObject['PRESSRELEASE'], "../../PressReleases.cfm"],
+	[sObject['TRADESHOW'], "../../Tradeshows.cfm"],
+	[sObject['PARTNER'], "../../partners.cfm"],
+	[sObject['SUSTAINLINK'], "../../ci/ciarticles.cfm?section=2"]
+]
+
+var anylinkmenu4={divclass:'anylinkmenu', inlinestyle:'', linktarget:'_new'} //Second menu variable. Same precaution.
+anylinkmenu4.items=[
+	[sObject['BMC'], "http://www.bostonmicromachines.com/"],
+	[sObject['IQE'], "http://www.idestaQE.com"],
+	[sObject['MENLO'], "http://www.menlosystems.com/"],
+	[sObject['PICO'], "../../images/pdf/PicoLuzPageforWeb.pdf"],
+	[sObject['PRAEV'], "http://www.praevium.com/"]
+]
+
+var anylinkmenu5={divclass:'anylinkmenu', inlinestyle:'', linktarget:''} //Second menu variable. Same precaution.
+anylinkmenu5.items=[
+	[sObject['LOGIN'], "../../login.cfm?lc=login"],
+	[sObject['SHOPCART'], "../../cart/basket/Showcott3.cfm"],
+	[sObject['REGISTER'], "../../login.cfm?lc=create"],
+	[sObject['FAQ'], "../../navigation.cfm?guide_id=2375"]
+	
+]
+
+var anylinkmenu6={divclass:'anylinkmenu', inlinestyle:'', linktarget:''} //First menu variable. Make sure "anylinkmenu1" is a unique name!
+anylinkmenu6.items=[
+	[sObject['NEEDOTABLES'], "tables.cfm"],
+	[sObject['SOURCEVIB'], "tables2.cfm"],
+	[sObject['THEORYTBLEVIB'], "tables3.cfm"],
+	[sObject['TBLEDESIGN'], "tables4.cfm"],
+	[sObject['THEORYVIBISO'], "tables5.cfm"],
+	[sObject['TBLEISODSGN '], "tables6.cfm"],
+	[sObject['CONCLUSION'], "tables7.cfm"]	
+]
+
+var anylinkmenu7={divclass:'anylinkmenu', inlinestyle:'', linktarget:''} //Second menu variable. Same precaution.
+anylinkmenu7.items=[
+	[sObject['LOGOUT'], "../../action.cfm?act=logout"],
+	[sObject['SHOPCART'], "../../cart/basket/Showcott3.cfm"],
+	[sObject['ACCOUNT'], "../../profiles/mythorlabs.cfm"],
+	[sObject['VENDORPACKING'], "../../Vendor/VendorPackingSlip.cfm"],
+	[sObject['FAQ'], "../../navigation.cfm?guide_id=2375"]
+	
+]
+
+var anylinkmenu8={divclass:'anylinkmenu', inlinestyle:'', linktarget:''} //Second menu variable. Same precaution.
+anylinkmenu8.items=[
+	[sObject['SHOPCART'], "../../cart/basket/Showcott3.cfm"],
+	[sObject['FAQ'], "../../navigation.cfm?guide_id=2375"]
+]
+
+
