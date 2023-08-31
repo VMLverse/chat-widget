269f827bbc7b775d474fe7ad306c4fc71522e100
Vimal Venugopal <vimalalwaysrocks@gmail.com>
2023-08-31 17:22:29 -0400

    new

---
+++ index_files/polyfill.js
@@ -0,0 +1,111 @@
+/* Array.from() polyfill */
+if (!Array.from) {
+	Array.from = (function () {
+		var toStr = Object.prototype.toString;
+		var isCallable = function (fn) {
+			return typeof fn === 'function' || toStr.call(fn) === '[object Function]';
+		};
+		var toInteger = function (value) {
+			var number = Number(value);
+			if (isNaN(number)) { return 0; }
+			if (number === 0 || !isFinite(number)) { return number; }
+			return (number > 0 ? 1 : -1) * Math.floor(Math.abs(number));
+		};
+		var maxSafeInteger = Math.pow(2, 53) - 1;
+		var toLength = function (value) {
+			var len = toInteger(value);
+			return Math.min(Math.max(len, 0), maxSafeInteger);
+		};
+
+		return function from(arrayLike/*, mapFn, thisArg */) {
+			var C = this;
+			var items = Object(arrayLike);
+
+			if (arrayLike == null) {
+				throw new TypeError("Array.from requires an array-like object - not null or undefined");
+			}
+
+			var mapFn = arguments.length > 1 ? arguments[1] : void undefined;
+			var T;
+
+			if (typeof mapFn !== 'undefined') {
+				if (!isCallable(mapFn)) {
+					throw new TypeError('Array.from: when provided, the second argument must be a function');
+				}
+
+				if (arguments.length > 2) {
+					T = arguments[2];
+				}
+			}
+
+			var len = toLength(items.length);
+			var A = isCallable(C) ? Object(new C(len)) : new Array(len);
+			var k = 0;
+			var kValue;
+
+			while (k < len) {
+				kValue = items[k];
+				if (mapFn) {
+					A[k] = typeof T === 'undefined' ? mapFn(kValue, k) : mapFn.call(T, kValue, k);
+				} else {
+					A[k] = kValue;
+				}
+				k += 1;
+			}
+
+			A.length = len;
+
+			return A;
+		};
+	}());
+}
+
+/* CustomEvent Polyfill */
+(function () {
+	function CustomEvent(event, params) {
+		params = params || {bubbles: false, cancelable: false, detail: undefined};
+		var evt = document.createEvent('CustomEvent');
+		evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
+		return evt;
+	}
+
+	CustomEvent.prototype = window.Event.prototype;
+	window.CustomEvent = CustomEvent;
+})();
+
+/* AddEventListener Polyfill */
+function bindEvent(element, eventName, eventHandler) {
+	if (element.addEventListener) {
+		element.addEventListener(eventName, eventHandler, false);
+	} else if (element.attachEvent) {
+		element.attachEvent('on'+eventName, eventHandler);
+	}
+}
+
+/* Array .includes() Polyfill */
+if (!Array.prototype.includes) {
+	Object.defineProperty(Array.prototype, "includes", {
+	  enumerable: false,
+	  value: function(obj) {
+		  var newArr = this.filter(function(el) {
+			return el == obj;
+		  });
+		  return newArr.length > 0;
+		}
+	});
+}
+
+/* String .includes() Polyfill */
+if (!String.prototype.includes) {
+	String.prototype.includes = function(search, start) {
+		'use strict';
+		if (typeof start !== 'number') {
+			start = 0;
+		}
+		if (start + search.length > this.length) {
+			return false;
+		} else {
+			return this.indexOf(search, start) !== -1;
+		}
+	};
+}
